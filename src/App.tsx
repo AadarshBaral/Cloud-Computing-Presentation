7a5c3e8f593655a270612d05e75863a016060b30
AadarshBaral <adarsmain101@gmail.com>
2025-06-26 09:39:15 +0545

    feat: changes in app

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>Cloud Computing Presentation</p>
+        <p>Cloud Tool</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
